52ea71b12eaa958fbd5831ff58dccb91117febf5
shunsukekyoya <kyoya@hb-style.co.jp>
2016-02-12 16:13:09 +0000

    heoku-challenge2

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,8 @@ class Message < ActiveRecord::Base
      # 内容は必須入力かつ2文字以上30文字以下
      validates :body , length: { minimum: 2, maximum: 30 } , presence: true
      
+class Player < ActiveRecord::Base
+  validates :age, numericality: true
+  validates :age, numericality: { only_integer: true }
+end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160202085256) do
+ActiveRecord::Schema.define(version: 20160212030958) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
